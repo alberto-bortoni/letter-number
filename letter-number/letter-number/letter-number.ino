190f0dd1e06313a2e554d42e7c25a89314d0f590
alberto bortoni <alb.deg@gmail.com>
2018-12-23 11:40:32 -0500

    This is aa change in the device's propose, it displays a clock now

--- letter-number/letter-number/letter-number.ino
+++ letter-number/letter-number/letter-number.ino
@@ -9,20 +9,26 @@
  *   - HT16K33 driver chip with 16x8 1.2" LED Matrix
  *   - Adafruit 5V 2.4A Switching Power Supply
  *
- *  This program displays a letter from the alphabet in one matrix
- *  and the corresponding number in the ohter (eg. A 1).
- *  This is made so that I can memorize the numbers and "add letters"
- *  as I walk arround on my day-to-day.
- *  The letters greater than 10, are fromed by adding the numbers in
- *  their 2 digit form, eg. K2,L3,S1.
+ *  This program displays a clock in 24h format in 5x3 numbers.
+ *  The upper secion of the 8x16 display contains code for day of
+ *  the week and day of the month in a 3x8 display each.
+ *  Day of the week is coded in binary, 3 buts, where 0 is Monday.
+ *  The day of the month is broken into BCD too but with a separate
+ *  counter for the 10ths (2bits, 0~3) and the units (4 bits, 0~9).
+ *  The display will turn off at night.
+ *  The button will toggle display on/off regardless of the time.
  *
- *  RTC is used to dim the brightness of the display at night and change
- *  letter displayed every 3 seconds at random.
+ *  If the board does not respond while programming, make sure
+ *  the arduino is started with admin privileges, that arduino
+ *  pro mini 328 16Mhz, and load the program. Then reset the board.
+ *  if it fails, try again.
  *
 */
 /*---------------------------------------------------------------*/
 
 #include <Wire.h>
+#include "arrays.h"
+#include <stdlib.h>
 #include "RTClib.h"
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
@@ -36,14 +42,11 @@ Adafruit_8x16matrix matrix = Adafruit_8x16matrix();
 
 RTC_DS3231 rtc;
 
-const int bttnPin  =  2;
-const int ledPin   = 13;
-const char abc[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const char numb[] = "12345678912345678912345678";
-int randomNumber = 0;
-int latch = 0;
-int nowtime = 0;
-int nowhour = 0;
+const int bttnPin = 2;
+const int ledPin  = 13;
+int latch         = 1;
+int nowtime       = 0;
+int lastMin       = 0;
 
 /***********************************************************/
 /*                  FUNCTION PROTOTYPES                    */
@@ -51,75 +54,103 @@ int nowhour = 0;
 void setup() {
 
   matrix.begin(0x70);
-  matrix.setTextSize(1);
   matrix.setTextWrap(false);
   matrix.setTextColor(LED_ON);
   matrix.setRotation(1);
   matrix.clear();
-  matrix.setBrightness(2);
-      
+  matrix.setBrightness(1);
+
   rtc.begin();
+  //following line sets the RTC to the date & time this sketch was compiled
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
   pinMode(ledPin, OUTPUT);
   pinMode(bttnPin, INPUT_PULLUP);
 
-  randomSeed(analogRead(0));
-
-  randomNumber = random(26);
-  disp(abc[randomNumber], numb[randomNumber]);
   delay(1000);
 }
 /*------------------------------------------------*/
 
-
-
 void loop() {
-
   DateTime now = rtc.now();
-  delay(3000);
 
-  nowhour = now.hour();
-  if(nowhour<6 || nowhour>21){
-    nowtime = 0;}   //night
+  if(now.hour()<6 || now.hour()>=23){
+    nowtime = 0;  //night
+  }
   else{
-    nowtime = 1;}   //day
+    nowtime = 1;  //day
+  }
 
-  switch (nowtime*(nowhour%3 + 1)){
-    case 0:     // night
-      disp(' ', ' ');
-      latch = 1;
-      break;
-    case 1:     //change
-      if(latch){
-        randomNumber = random(26);
-        disp(abc[randomNumber], numb[randomNumber]);
-        latch = 0;
-      }
-      break;
-    case 2:     //nothing
-      latch = 1;
-      break;
-    case 3:     //nothing
-      break;
-    }
-  
+
+  if((nowtime==1 && latch==1)||(nowtime==0 && latch==0)){
+    disp(now.dayOfTheWeek(), now.day(), now.hour(), now.minute());
+  }
+  else{
+    matrix.clear();
+    matrix.writeDisplay();
+    delay(1000);
+  }
+
+
   if(digitalRead(bttnPin) == 0){
-    randomNumber = random(26);
-    disp(abc[randomNumber], numb[randomNumber]);
-    delay(3000);
-    }
+    latch = latch==1? 0:1;
+  }
+
 }
 /*------------------------------------------------*/
 
+void disp(int tweek, int tday, int thour, int tminute){
+
+  int k;
+  int low;
+  int hi;
+  uint8_t daysAr[2];
+  uint8_t timeAr[5];
+
+  if(abs(lastMin-tminute)!=0){
+    matrix.clear();
+
+    //display day of week
+    daysAr[0] = week[tweek];
+    daysAr[1] = daysAr[0];
+    matrix.drawBitmap(0, 0, daysAr, 8, 2, LED_ON);
 
 
-void disp( char letter, char number){
-  matrix.clear();
-  matrix.setCursor(1,0);
-  matrix.print(letter);
-  matrix.setCursor(10,0);
-  matrix.print(number);
-  matrix.writeDisplay();
+    //number of day
+    low = tday%10;
+    hi = (tday-low)/10;
+    daysAr[0] = daysHi[hi] | daysLow[low];
+    daysAr[1] = daysAr[0];
+    matrix.drawBitmap(8, 0, daysAr, 8, 2, LED_ON);
+
+
+    //hours
+    low = thour%10;
+    hi = (thour-low)/10;
+
+    for(k=0; k<5; k++){
+      if(hi!=0){
+        timeAr[k] = numbHi[hi][k] | numbLow[low][k];
+      }else{
+        timeAr[k] = zeros[k] | numbLow[low][k];
+      }
+    }
+    matrix.drawBitmap(0, 3, timeAr, 8, 5, LED_ON);
+
+
+    //minutes
+    low = tminute%10;
+    hi = (tminute-low)/10;
+
+    for(k=0; k<5; k++){
+      timeAr[k] = numbHi[hi][k] | numbLow[low][k];
+    }
+    matrix.drawBitmap(8, 3, timeAr, 8, 5, LED_ON);
+
+
+    matrix.writeDisplay();
+    delay(400);
+  }
 }
 /*------------------------------------------------*/
 
